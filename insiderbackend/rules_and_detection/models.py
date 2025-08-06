baaf0c2d7c5986baf07524f421a0e3a4a8f9d9d5
Kris-Kirimi <christine.kirimi@strathmore.edu>
2025-08-06 16:27:18 +0300

    Add employee data and update AccessPolicy string representation

--- insiderbackend/rules_and_detection/models.py
+++ insiderbackend/rules_and_detection/models.py
@@ -13,7 +13,7 @@ class AccessPolicy(models.Model):
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.employee_id.username} - {self.resource} - {self.allowed_actions}"
+        return f"{self.employee_id.username} - {self.resource_name} - {self.allowed_actions}"
 
 class Anomaly(models.Model):
     anomaly_id = models.CharField(max_length=255, unique=True)
